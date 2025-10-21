ceac7ae6550a87b63430a9ffef5253b0b6f2ed09
114360204 <kjoe6698@gmail.com>
2025-10-22 02:30:06 +0800

    first upload

--- 2.25.c
+++ 2.25.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
 
 int main(void) {
+    // 第一個英文大寫：I
+    printf("I                   I   \n");
+    printf("I                   I   \n");
+    printf("I                   I   \n");
+    printf("I                   I   \n");
+    printf("IIIIIIIIIIIIIIIIIIIII   \n");
+    printf("I                   I   \n");
+    printf("I                   I   \n");
+    printf("I                   I   \n");
+    printf("I                   I   \n\n");
 
-    printf("I                   I\n");
-    printf("I                   I\n");
-    printf("I                   I\n");
-    printf("I                   I\n");
-    printf("IIIIIIIIIIIIIIIIIIIII\n");
-    printf("I                   I\n");
-    printf("I                   I\n");
-    printf("I                   I\n");
-    printf("I                   I\n\n");
+    // 第二個英文大寫：A
+    printf("AAAAAAAAAAAAA           \n");
+    printf("     A        A         \n");
+    printf("     A          A       \n");
+    printf("     A            A     \n");
+    printf("     A              A   \n");
+    printf("     A            A     \n");
+    printf("     A          A       \n");
+    printf("     A        A         \n");
+    printf("AAAAAAAAAAAAAA          \n\n");
+        
+    // 第三個英文大寫：N
+    printf("NNNNNNNNNNNNNNNNNNNNNN  \n");
+    printf("                     N  \n");
+    printf("                   N    \n");
+    printf("                 N      \n");
+    printf("               N        \n");
+    printf("             N          \n");
+    printf("           N            \n");
+    printf("         N              \n");
+    printf("       N                \n");
+    printf("     N                  \n");
+    printf("   N                    \n");
+    printf(" N                      \n");
+    printf("NNNNNNNNNNNNNNNNNNNNNN  \n");
 
-    // 第二個圖形：橢圓
-    printf("AAAAAAAAAAAAA         \n");
-    printf("     A        A       \n");
-    printf("     A          A     \n");
-    printf("     A            A   \n");
-    printf("     A              A \n");
-    printf("     A            A   \n");
-    printf("     A          A     \n");
-    printf("     A        A       \n");
-    printf("AAAAAAAAAAAAAA      \n\n");
-
-    // 第三個圖形：十字
-    printf("NNNNNNNNNNNNNNNNNNNNNN\n");
-    printf("                     N\n");
-    printf("                   N  \n");
-    printf("                 N    \n");
-    printf("               N      \n");
-    printf("             N        \n");
-    printf("           N          \n");
-    printf("         N            \n");
-    printf("       N              \n");
-    printf("     N                \n");
-    printf("   N                  \n");
-    printf(" N                    \n");
-    printf("NNNNNNNNNNNNNNNNNNNNNN\n");
 
     return 0;
 }
